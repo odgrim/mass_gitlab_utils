128caa3983504e60d26d2b7e1bbbdd26d836c854
odgrim <odgrim.post@gmail.com>
2015-03-04 16:26:00 -0800

    first working revision

--- gitlab.bash
+++ gitlab.bash
@@ -11,13 +11,13 @@ function __mass_gitlab_utils_generate_request() {
 function __mass_gitlab_utils_get_last_pagination_page_of_object() {
     # gitlab apis are paginated. Grab the first page of an object and extract the "last" page number
     paginated_object="$1"
-    object_pages=`__mass_gitlab_utils_generate_request "GET" "${paginated_object}" "-I" | egrep "^Link:.*"`
+    object_pages=`__mass_gitlab_utils_generate_request "GET" "${paginated_object}" "-I" \?per_page=100 | egrep "^Link:.*"`
     # Regex to match the "last page"
-    regex_pattern='^Link:.+page=([0-9]+)&per_page=0>\; rel="last".*$'
+    regex_pattern='^Link:.+page=([0-9]+)&per_page=100>\; rel="last".*$'
 
     # Grab the "last page"
     [[ $object_pages =~ $regex_pattern ]]
-    echo "${BASH_REMATCH[1]-'1'}"
+    echo "${BASH_REMATCH[1]-1}"
 }
 
 function __mass_gitlab_utils_concatenate_request_object() {
@@ -28,7 +28,7 @@ function __mass_gitlab_utils_concatenate_request_object() {
     # Fetch and concatenate all pages of groups
     all_objects="[]"
     for ((i=1;i<=$max_pages;i++)); do
-        this_request=`__mass_gitlab_utils_generate_request "GET" "${requested_object}" "" \?page=${i}`
+        this_request=`__mass_gitlab_utils_generate_request "GET" "${requested_object}" "" \?page=${i}\&per_page=100`
 	all_objects=`__mass_gitlab_utils_flatten_json "$this_request" "$all_objects"`
     done
      
@@ -73,7 +73,20 @@ function __mass_gitlab_utils_get_projects_from_group() {
 }
 
 function __mass_gitlab_utils_update_project() {
-echo "asdf"    
+   local_path=$1
+   repo_path=$2
+   repo_url=$3
+   if [[ ! -d "${local_path}/${repo_path}" ]]; then
+       echo "Cloning new project repo in ${local_path}/${repo_path}..."
+       pushd "${local_path}/${group}" > /dev/null
+       git clone $repo_url
+       popd > /dev/null
+   else
+       echo "Updating existing repo at ${local_path}/${repo_path}..."
+       pushd "${local_path}/${repo_path}" > /dev/null
+       git pull origin
+       popd > /dev/null
+    fi
 }
 
 function __mass_gitlab_utils_update() {
@@ -81,25 +94,17 @@ function __mass_gitlab_utils_update() {
    git_groups=( $(git config --get-all mass-gitlab-utils.group ) )
    IFS=$'\n'
    set -f
+   echo "Updating ${#git_groups[@]} groups..."
    for group in ${git_groups[@]}; do
        if [[ ! -d "${local_path}/${group}" ]]; then
            mkdir -p "${local_path}/${group}"
        fi
-       group_projects=( $(__mass_gitlab_utils_get_projects_from_group "sddc") )
+       group_projects=( $(__mass_gitlab_utils_get_projects_from_group "$group") )
+       echo "Updating ${#group_projects[@]} projects in group $group..."
        for project in ${group_projects[@]}; do
-           repo_path=`echo $group | jq -r ".path_with_namespace"`
-           repo_url=`echo $group | jq -r ".ssh_url_to_repo"`
-           echo "updating repo at ${local_path}/${repo_url}..."
-	   if [[ ! -d "${local_path}/${repo_path}" ]]; then
-               echo "updating repo at ${local_path}/${group}..."
-	       pushd "${local_path}/${group}"
-	       git clone $repo_url
-	       popd
-           else 
-               pushd "${local_path}/${repo_path}"
-	       git pull --all
-	       popd
-	   fi
+           repo_path=`echo $project | jq -r ".path_with_namespace"`
+           repo_url=`echo $project | jq -r ".ssh_url_to_repo"`
+	   __mass_gitlab_utils_update_project "$local_path" "$repo_path" "$repo_url"
        done
    done
 }
